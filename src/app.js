7be7da2c54f6160aa682161989476961df884f2e
Esteban Dadul <edadul@uninorte.edu.co>
2025-05-02 12:04:48 -0500

    CORS

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.disable('x-powered-by')
 const allowedOrigins = [
   'http://localhost:5173', 
   'https://db-project02.onrender.com',
-  'https://db-project02.vercel.app/'
+  'https://db-project02.vercel.app'
 ]
 
 // ---- Middleware ----
